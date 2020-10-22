1f0110b097370e0fa2aff3578e7a9661c6c7ffb2
maks <mojpo4tovik@gmail.com>
2020-10-22 14:34:44 +0300

    number format

---
+++ webpack.config.js
@@ -0,0 +1,26 @@
+const path = require('path');
+
+module.exports = {
+  entry: './src/index.ts',
+  output: {
+    path: path.resolve(__dirname, 'dist'),
+    filename: 'revogrid-number.js',
+    library: 'revogridNumber',
+    libraryTarget: 'umd',
+  },
+  externals: {
+    numbro: 'numbro',
+  },
+  resolve: {
+    extensions: [ '.tsx', '.ts', '.js' ],
+  },
+  module: {
+    rules: [
+      {
+        test: /\.tsx?$/,
+        use: 'ts-loader',
+        exclude: /node_modules/,
+      },
+    ],
+  },
+};
