462e24b85d39c36f9aa9c807a18eefc165cd67fc
revolist <maksim.k@revolist.eu>
2020-10-23 15:37:16 +0300

    numeral

--- src/index.ts
+++ src/index.ts
@@ -1,29 +1,21 @@
 import { RevoGrid } from '@revolist/revogrid/dist/types/interfaces';
 import { VNode } from '@revolist/revogrid/dist/types/stencil-public-runtime';
-import Numbro from 'numbro';
+import numeral from 'numeral';
 
-const defaultFormat: Numbro.Format = {
-    thousandSeparated: true,
-    mantissa: 2,
-    optionalMantissa: true
-};
+const defaultFormat: string = '0,0[.]00';
 
 export default class NumberColumnType {
-    private readonly numbroNumberFormat: Numbro.Format;
-    constructor(numbroNumberFormat?: Numbro.Format) {
-        if (!numbroNumberFormat) {
-            this.numbroNumberFormat = defaultFormat;
+    private readonly numberFormat: string;
+    constructor(format?: string, private emiter?: (event: string, instance: Numeral) => void) {
+        if (!format) {
+            this.numberFormat = defaultFormat;
         } else {
-            this.numbroNumberFormat = numbroNumberFormat;
+            this.numberFormat = format;
         }
     }
     columnProperties = (): RevoGrid.CellProps => ({ class: { ['align-center']: true }});
 
     cellProperties = (): RevoGrid.CellProps => ({ class: { ['align-right']: true } });
-
-    formated(val: number): string {
-        return Numbro(val).format(this.numbroNumberFormat)
-    }
 
     cellTemplate = (_h: RevoGrid.HyperFunc<VNode>, p: RevoGrid.ColumnDataSchemaModel): string => {
         const parsed = parseInt(p.model[p.prop], 10);
@@ -32,4 +24,17 @@ export default class NumberColumnType {
         }
         return this.formated(parsed);
     };
+
+    formated(val: number): string {
+        const num = numeral(val);
+        if (this.emiter) {
+            this.emiter('beforeValueFormatted', num);
+        }
+        return num.format(this.numberFormat)
+    }
+    
+    /** Get numeral instance in case you don't want to add any in your project */
+    static getNumeralInstance(): Numeral {
+        return numeral;
+    }
 }
