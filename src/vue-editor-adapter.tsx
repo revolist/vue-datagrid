0d27c049c696c1f81bd86e3bcd2c6be73be66564
revolist <maksim.k@revolist.eu>
2021-02-04 20:25:00 -0500

    adapter fix

--- src/vue-editor-adapter.tsx
+++ src/vue-editor-adapter.tsx
@@ -4,33 +4,42 @@ import { VueConstructor } from 'vue/types/umd';
 import { vueTemplateConstructor } from './vue-template';
 
 export default class VueEditorAdapter {
-    public element: Element|null = null;
-    public editCell: Edition.EditCell|null = null;
-    private vueEl: Vue|undefined;
+	public element: Element|null = null;
+	public editCell: Edition.EditCell|null = null;
+	private vueEl: Vue|undefined;
 
-    constructor(
-        private VueEditorConstructor: VueConstructor,
-        public column: RevoGrid.ColumnDataSchemaModel,
-        private save: Function,
-        private close: Function
-    ) {}
+	constructor(
+			private VueEditorConstructor: VueConstructor,
+			public column: RevoGrid.ColumnDataSchemaModel,
+			private save: Function,
+			private close: Function
+	) {}
 
-    // optional, called after editor rendered
-    componentDidRender() {}
+	// optional, called after editor rendered
+	componentDidRender() {}
 
-    // optional, called after editor destroyed
-    disconnectedCallback() {
-        this.vueEl?.$destroy();
-        this.vueEl = undefined;
-    }
+	// optional, called after editor destroyed
+	disconnectedCallback() {
+			this.vueEl?.$destroy();
+			this.vueEl = undefined;
+	}
 
-    render(h: RevoGrid.HyperFunc<VNode>) {
-        return <span ref={(el: HTMLElement) => {
-            console.log(this.save, this.close);
-          this.vueEl = vueTemplateConstructor(this.VueEditorConstructor, el, {
-            ...this.editCell,
-            save: this.save,
-            close: this.close
-        })}}/>;
-    }
+	render(h: RevoGrid.HyperFunc<VNode>) {
+		return <span ref={(e: HTMLElement) => this.renderAdapter(e)}/>;
+	}
+
+	private renderAdapter(el?: HTMLElement) {
+		if (!el) {
+			return;
+		}
+		const template = vueTemplateConstructor(this.VueEditorConstructor, el, {
+			...this.editCell,
+			save: this.save,
+			close: this.close
+		});
+		if (!template) {
+			return;
+		}
+		this.vueEl = template;
+	}
 }
