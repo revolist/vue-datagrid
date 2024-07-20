cc27adf5564896e4b50ff6a377503d55f6472c8e
maks <mojpo4tovik@gmail.com>
2024-07-20 22:00:44 +0100

    feat: support internal modules revogrid

--- lib/index.ts
+++ lib/index.ts
@@ -3,7 +3,7 @@ import { defineCustomElements } from '@revolist/revogrid/loader';
 export { Template as VGridVueTemplate, VGridVueTemplateConstructor } from './renderer';
 export { Editor as VGridVueEditor } from './editor';
 export { type EditorType } from './editor.adapter';
-export type * from '@revolist/revogrid';
+export * from '@revolist/revogrid';
 
 export const VGrid = (async () => {
     await (defineCustomElements() as unknown as Promise<void>);
